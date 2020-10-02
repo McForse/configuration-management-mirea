91042898db8d58a4127d38db3c5184acb2b940c6
Danil Vorozheykin <danilvda@gmail.com>
2020-10-02 11:48:25 +0300

    Update parser output

--- ДЗ 3-4/MyParser.py
+++ ДЗ 3-4/MyParser.py
@@ -1,43 +1,39 @@
 from sly import Parser
 from MyLexer import MyLexer
+from copy import deepcopy as copy
+import json
 
 
 class MyParser(Parser):
     tokens = MyLexer.tokens
 
     @_('"(" expr_sequence ")"')
-    def expr_sequence(self, p):
+    def statement(self, p):
         return p.expr_sequence
 
     @_('expr "(" expr ")"')
     def expr_sequence(self, p):
-        return [p.expr0, [p.expr1]]
+        return {p.expr0: [p.expr1]}
 
     @_('expr "(" expr_sequence ")"')
     def expr_sequence(self, p):
-        return [p.expr, p.expr_sequence]
+        return {p.expr: [p.expr_sequence]}
 
     @_('expr_sequence "(" expr ")"')
     def expr_sequence(self, p):
-        items_list = list(p.expr_sequence)
-        items_list.append([p.expr])
-        return items_list
+        items_dict = p.expr_sequence
+        key = list(items_dict.keys())[0]
+        items_list = list(items_dict[key])
+        items_list.append(p.expr)
+        return {key: copy(items_list)}
 
     @_('expr_sequence "(" expr_sequence ")"')
     def expr_sequence(self, p):
-        items_list = list(p.expr_sequence0)
+        items_dict = p.expr_sequence0
+        key = list(items_dict.keys())[0]
+        items_list = list(items_dict[key])
         items_list.append(p.expr_sequence1)
-        return items_list
-
-    @_('expr_sequence expr')
-    def expr_sequence(self, p):
-        items_list = list(p.expr_sequence)
-        items_list.append(p.expr)
-        return items_list
-
-    @_('expr expr')
-    def expr_sequence(self, p):
-        return [p.expr0, p.expr1]
+        return {key: copy(items_list)}
 
     @_('NUMBER')
     def expr(self, p):
@@ -48,15 +44,28 @@ class MyParser(Parser):
         return p.NAME
 
 
+if __name__ == '__main__':
+    lexer = MyLexer()
+    parser = MyParser()
+
+    try:
+        data = '''
+        (var1 (var3 (var5 (var6))) (var7 (var8 (543534))) (var9))
+        '''
+        result = parser.parse(lexer.tokenize(data))
+        print(result)
+        print(json.dumps(result))
+    except EOFError:
+        pass
+
 '''
-Grammar
+Grammar (BNF)
 ------------------------
 
-expr_sequence : expr expr
+statement : ( expr_sequence )
+
 expr_sequence | expr ( expr )
 expr_sequence | expr ( expr_sequence )
-expr_sequence | ( expr_sequence )
-expr_sequence | expr_sequence expr
 expr_sequence | expr_sequence ( expr )
 expr_sequence | expr_sequence ( expr_sequence )
 
