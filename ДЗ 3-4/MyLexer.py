369d5b126385eec78d06331b7ce38125ff1d3840
Danil Vorozheykin <danilvda@gmail.com>
2020-10-04 14:01:41 +0300

    Reformat files

--- ДЗ 3-4/MyLexer.py
+++ ДЗ 3-4/MyLexer.py
@@ -33,18 +33,3 @@ class MyLexer(Lexer):
     def error(self, t):
         print('\033[91m' + 'Line %d: Bad character %r' % (self.lineno, t.value[0]) + '\033[0m')
         self.index += 1
-
-
-if __name__ == '__main__':
-    data = '''
-    (groups
-        ("IKBO-01-19")
-        ("IKBO-02-19")
-        ("IKBO-03-19")
-        ("IKBO-04-19")
-        ("IKBO-05-19")
-    )
-    '''
-    lexer = MyLexer()
-    for token in lexer.tokenize(data):
-        print(token)
