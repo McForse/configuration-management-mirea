cc3b9015e98d4db3ca7b2b475654ec13a4cff9b0
Danil Vorozheykin <danilvda@gmail.com>
2020-12-02 22:34:06 +0300

    Add Practice 13-14

---
+++ ПР 13-14/noweb/insertionSort.py
@@ -0,0 +1,8 @@
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        item = arr[i]
+        j = i - 1
+        while j >= 0 and arr[j] > item:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = item
